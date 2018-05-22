ece3f790a69044156345471018596b36847e0670
anton feldmann <anton.feldmann@gmail.com>
2018-05-22 16:50:39 +0900

    works

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
-var http = require('http');
-
-var break_loop = false;
-var stack = [];
-
 var request = require('sync-request');
-var res = request('GET', "http://"+process.env.URL+":80/KAREL/DICTINFO?dict=" + process.env.DICT + "&dictno=1");
-console.log(res.getBody());
+
+for (var i = 0; true; i++){
+    var res = request('GET', "http://"+process.env.URL+":80/KAREL/DICTINFO?dict=" + process.env.DICT + "&dictno=" + i.toString());
+
+    var dictentry = JSON.parse(res.getBody('utf8'));
+
+    if (typeof dictentry.id !== 'undefined' ){
+	console.log(dictentry);
+    }else{
+	break;
+    }
+}
