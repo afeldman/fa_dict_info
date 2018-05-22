79668e0e74c16d24e66f4aa8ca8daaf3a1b2cfc4
anton feldmann <anton.feldmann@gmail.com>
2018-05-22 16:42:21 +0900

    read request sync

--- index.js
+++ index.js
@@ -1,44 +1,9 @@
 require('dotenv').config();
 var http = require('http');
 
+var break_loop = false;
 var stack = [];
-stack.push('last');
 
-function get_top() {
-    var top = stack.pop();
-    if (top != 'last')
-	collection(top);
-}
-
-
-function collection(item){
-    var path = "http://"+process.env.URL+":80/KAREL/DICTINFO?dict=" + process.env.DICT + "&dictno=" + item;
-
-    console.log(path);
-    
-    http.get(path, function(res) {
-	console.log("Got response: " + res.statusCode);
-	
-	var data = '';
-	
-	res.on("data", function(chunk) {
-	    data += chunk;
-	});
-	
-	res.on('end', function() {
-	    console.log(data);
-	});
-	
-    }).on('error', function(e) {
-	console.log("Got error: " + e.message);
-    });
-}
-
-for (var i = 0; i<=100;  i++) {
-    stack.push( i.toString() );
-}
-
-
-get_top();
-
-console.log();
+var request = require('sync-request');
+var res = request('GET', "http://"+process.env.URL+":80/KAREL/DICTINFO?dict=" + process.env.DICT + "&dictno=1");
+console.log(res.getBody());
